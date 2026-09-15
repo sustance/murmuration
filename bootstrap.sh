d5c87672c657fade61f623d553360654e09f7e72
Kym Michael <kym.michael@gmail.com>
2026-09-16 02:48:47 +0800

    Modify bootstrap.sh to use starling.py
    Updated script to download and execute starling.py instead of status_slaves.py. Adjusted comments and curl commands for clarity.

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,13 +1,25 @@
 #!/usr/bin/sh
-# this may not be correct over all linux and bsd, but is it aliased everywhere?
-# curl or python3 bay not always work without path
+# this #! may not be correct over all linux and bsd, but is it aliased everywhere?
+# curl or python3 may not always work without path ...test
 
-curl https://raw.githubusercontent.com/sustance/murmuration/refs/heads/main/status_slaves.py \
-  -o /home/$USER/public_html/status_slaves.py
-python3 /home/$USER/public_html/status_slaves.py
+curl https://raw.githubusercontent.com/sustance/murmuration/refs/heads/main/.local/bin/starling.py \
+     -o /home/$USER/.local/bin/starling.py
+python3 /home/$USER/.local/bin/starling.py  
+# this creates a system summary called starling.json, later colleted as snapshot of the murmuration
 
-curl https://raw.githubusercontent.com/sustance/configs/refs/heads/main/ mm -o /home/$USER/.local/bin/mm 
+curl https://sustance.github.io/bookmark.html -o /home/$USER/.w3m/bookmark.html 
 
-#Note
-# This is pulled by chron each day at the same UTC time on every computer in the swarm then 
-# The computers are mostly set to their local time
+
+# either select different file for each server by $HOST, or, create and edit index file on each server
+#curl https://raw.githubusercontent.com/sustance/murmuration/refs/heads/main/public_html/index$HOST.php 
+#     -o /home/$USER/public_html/index.php 
+#curl https://raw.githubusercontent.com/sustance/murmuration/refs/heads/main/public_html/index$HOST.html 
+#     -o /home/$USER/public_html/bin/index.html 
+
+
+curl https://raw.githubusercontent.com/sustance/configs/refs/heads/main/mm -o /home/$USER/.local/bin/mm 
+
+#Notes
+# This is pulled by chron each day (optomistically at the same UTC time) on every computer in the swarm then 
+# The computers are mostly set to their local time 
+# as cron cannot do this timing perhaps maybe tolerat the time spread from HK to sanfrancisco
